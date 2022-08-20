636dece04fe80217edd9a01b50d8a6c169269d7a
Lazytangent <peter.tn.mai@gmail.com>
2022-08-20 15:16:19 -0500

    Begin planning of parsing messages

--- src/cli.rs
+++ src/cli.rs
@@ -13,7 +13,8 @@ pub fn run() {
         get_config_file_contents(config::PATH_TO_CONFIG_DIR, config::CONFIG_FILE_NAME).unwrap();
     let paths = config::generate_list_of_paths(contents);
 
-    get_status_from_paths(paths);
+    let outputs = get_status_from_paths(paths);
+    parse_outputs(outputs);
 }
 
 fn run_command(cmd: &mut Command, dir: &str) -> Output {
@@ -31,17 +32,33 @@ fn get_config_file_contents(path: &str, filename: &str) -> io::Result<String> {
     Ok(contents)
 }
 
-fn get_status_from_paths(paths: Vec<String>) {
+struct Outputs {
+    stdout: String,
+    stderr: String,
+}
+
+fn get_status_from_paths(paths: Vec<String>) -> Vec<Outputs> {
     let mut cmd = Command::new("git");
     let cmd = cmd.arg("status");
 
+    let mut outputs: Vec<Outputs> = Vec::new();
+
     for path in paths {
         let output = run_command(cmd, &path);
-        let stdout = str::from_utf8(&output.stdout).unwrap();
-        let stderr = str::from_utf8(&output.stderr).unwrap();
-
+        let stdout = str::from_utf8(&output.stdout).unwrap().to_string();
+        let stderr = str::from_utf8(&output.stderr).unwrap().to_string();
         debug!("Output:\n{}", stdout);
         debug!("Error:\n{}", stderr);
         debug!("---------------------------");
+
+        let output = Outputs { stdout, stderr };
+        outputs.push(output);
     }
+
+    outputs
+}
+
+fn parse_outputs(outputs: Vec<Outputs>) {
+    // TODO: Parse outputs using Regex to figure out which repositories need to be looked at
+    // TODO: Which repos can be safely ignored as part of a group ignore message
 }
