b8f42b133d6d5e55539aa01a9dd2e4dca35fac46
Lazytangent <peter.tn.mai@gmail.com>
2022-08-20 13:51:14 -0500

    Add logging

--- src/cli.rs
+++ src/cli.rs
@@ -1,22 +1,22 @@
-use std::process::{Command, Output};
+use std::{process::{Command, Output}, str};
+
+use log::debug;
 
 use crate::config;
 
 pub fn run() {
-    // run_command("ls", config::PATH_TO_CONFIG_DIR);
-
     let mut path = config::parse_path_with_tilde(config::PATH_TO_CONFIG_DIR).unwrap();
     path.push_str(config::CONFIG_FILE_NAME);
     let contents = config::read_config_file(&path).unwrap();
-    // debug!("Contents:\n{:?}", contents);
+    debug!("Contents:\t{:?}", contents);
 
     let paths = config::generate_list_of_paths(contents);
-    // debug!("Paths: {:?}", paths);
+    debug!("Paths: {:?}", paths);
     let mut cmd = Command::new("git");
     let cmd = cmd.arg("status");
 
     let output = run_command(cmd, &paths[0]);
-    println!("Output: {:?}", output);
+    print!("Output:\n{}", str::from_utf8(&output.stdout).unwrap());
 }
 
 fn run_command(cmd: &mut Command, dir: &str) -> Output {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use std::env;
+
+use pretty_env_logger;
+
 use checkrs::cli;
 
 fn main() {
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info");
+    }
+
+    pretty_env_logger::init_timed();
+
     cli::run();
 }
